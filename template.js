cf4eaf369bad05c8c425bc1fd4275ad9998d6580
Tony Herbert <tonyherbert22@gmail.com>
2022-04-05 16:55:11 -0500

    start new codewar

--- template.js
+++ template.js
@@ -1,20 +1,6 @@
-let bobsFollowers = ['Tony', 'Jake', 'Eric', 'Sean']
+function noSpace(x){
 
-let tinasFollowers = ['Jake', 'Taylor', 'Eric']
-
-let mutualFollowers = []
-
-for(let i = 0; i < bobsFollowers.length; i++){
-  for(let j = 0; j < tinasFollowers.length; i++){
-    if(bobsFollowers[i] === tinasFollowers[j] ) {
-      console.log(mutualFollowers.push[j])
-    }
-  }
 }
-
-
-
-
 
 
 
