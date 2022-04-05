ec75ac0534ac704f1c31a595fe6b05439aa2a75d
Tony Herbert <tonyherbert22@gmail.com>
2022-04-05 17:10:16 -0500

    Completed a new Codewar

--- template.js
+++ template.js
@@ -1,7 +1,9 @@
 function noSpace(x){
-
+  let str = x.split(' ').join('')
+  console.log(str);
 }
 
 
-
+noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd')
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B')
 
