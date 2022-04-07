36e61cd00448ba157022fc244feee9a4c4a28e5d
Tony Herbert <tonyherbert22@gmail.com>
2022-04-07 17:00:15 -0500

    end of day codewars commit

---
+++ 8kyu Solutions/8kyuCodeUnderPressure1.js
@@ -0,0 +1,13 @@
+
+function doubleInteger(i) {
+  return i*2
+}
+
+
+
+console.log(doubleInteger(2))   
+// console.log(doubleInteger('Johnny'))   
+
+
+
+
---
+++ 8kyu Solutions/8kyuJennySecretMessage.js
@@ -0,0 +1,13 @@
+
+function greet(name){
+  return(name === "Johnny") ? "Hello, my love!" : 'Hello, ' + name + '!';
+}
+
+
+
+console.log(greet('Jim'))   
+console.log(greet('Johnny'))   
+
+
+
+
