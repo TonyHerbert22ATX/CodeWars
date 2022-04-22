7ab3db8c90c354e87e4eb3fa61f137d8cba68d5c
Tony Herbert <tonyherbert22@gmail.com>
2022-04-22 13:56:06 -0500

    another 8kyu solution

---
+++ 8kyu Solutions/8kyuDebugSayHello.js
@@ -0,0 +1,6 @@
+function sayHello(name) {
+  return `Hello, ${name}`;
+}
+
+console.log(sayHello('Mr.Spock'));
+// console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']));
