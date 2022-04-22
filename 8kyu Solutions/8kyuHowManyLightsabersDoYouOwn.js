606fd5402ce83cfd03d0d17650bf26e96519435b
Tony Herbert <tonyherbert22@gmail.com>
2022-04-22 14:14:29 -0500

    add solutions

---
+++ 8kyu Solutions/8kyuHowManyLightsabersDoYouOwn.js
@@ -0,0 +1,9 @@
+function howManyLightsabersDoYouOwn(name) {
+  return name === "Zach" ? 18 : 0;
+}
+
+console.log(howManyLightsabersDoYouOwn());
+console.log(howManyLightsabersDoYouOwn("Adam"));
+console.log(howManyLightsabersDoYouOwn("Zach"));
+
+
