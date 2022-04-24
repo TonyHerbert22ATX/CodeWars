1cc5d278d0ea93e295a545708fac8940f87dd040
Tony Herbert <tonyherbert22@gmail.com>
2022-04-24 14:03:51 -0500

    a few more solutions

---
+++ 8kyu Solutions/8kyuTrainingJS1CreateFirstJSFunction.js
@@ -0,0 +1,8 @@
+function helloWorld(){
+  var str = 'Hello World!'
+  console.log(str);
+}
+
+console.log(helloWorld());
+// console.log(mouthSize("ant bear"));
+// console.log(mouthSize("alligator"));
