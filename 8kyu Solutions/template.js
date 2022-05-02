4faa9feed3a4e3d4c7e1ba85640c4bb288ea15c4
Tony Herbert <tonyherbert22@gmail.com>
2022-05-02 15:38:04 -0500

    end of day push

---
+++ 8kyu Solutions/template.js
@@ -0,0 +1,7 @@
+function swapValues(args) {
+  return args.reverse()
+}
+
+console.log(swapValues([1,2]));
+// console.log(swapValues(0.5));
+// console.log(swapValues(1));
--- template.js
+++
@@ -1,7 +0,0 @@
-function plural(n) {
-  return (n !== 1)?true:false
-}
-
-console.log(plural(0));
-console.log(plural(0.5));
-console.log(plural(1));
