8dad3363a0e0a7eeddbdfeb993c86f01234f16fe
Tony Herbert <tonyherbert22@gmail.com>
2022-06-15 16:02:21 -0500

    another solution

---
+++ 8kyu Solutions/8kyuPlishAlphabet.js
@@ -0,0 +1,14 @@
+function correctPolishLetters(string) {
+    return string
+        .replace(/ą/g, "a")
+        .replace(/ć/g, "c")
+        .replace(/ę/g, "e")
+        .replace(/ł/g, "l")
+        .replace(/ń/g, "n")
+        .replace(/ó/g, "o")
+        .replace(/ś/g, "s")
+        .replace(/ź/g, "z")
+        .replace(/ż/g, "z");
+}
+
+console.log(correctPolishLetters("Jędrzej Błądziński"));
