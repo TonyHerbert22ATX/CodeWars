54a4ef60e2bcfc0c14639b2d3bddd0a54e5de73b
Tony Herbert <tonyherbert22@gmail.com>
2022-06-14 19:10:58 -0500

    another solution

---
+++ 8kyu Solutions/8kyuClassicHelloWorld.js
@@ -0,0 +1,9 @@
+class Solution {
+    main() {
+        console.log("Hello World!");
+    }
+}
+let solution = new Solution();
+
+let ab = new Solution();
+console.log(ab);
--- template.js
+++ template.js
@@ -1,11 +1,1 @@
-function nextId(ids) {
-    let num = 0;
-    while (ids.includes(num)) {
-        num++;
-    }
-    return num
-}
-
-console.log(nextId([0, 1, 2, 3, 5]));
-console.log(nextId([5, 0, 1, 5, 5]));
-console.log(nextId([0, 1, 2, 3, 4,5,6,7,8,9,10]));
+function getDrinkByProfession(param) {}
