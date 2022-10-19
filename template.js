41f7ab3d949ae6cf0bfdc70707131c3e7071a3d6
Tony Herbert <tonyherbert22@gmail.com>
2022-10-19 10:50:19 -0500

    daily codewar

--- template.js
+++ template.js
@@ -1,12 +1,17 @@
-String.prototype.isLetter = function() {
-return /^[a-z]$/i.test(this);
+// Given an [] , min 3, all #'s, repeats allowed  && size of MAX
+
+// find the product * of MAX #'s && return total #
+
+
+
+
+function maxProduct(array, size){
+  //sort array
+  let sortArr = array.sort((a,z)=> a - z)
+  console.log(sortArr);
+  // grab size of max numbers
+  return sortArr.splice(-size).reduce((acc, concurrent )=> acc * concurrent, 1)
 }
 
-console.log(''.isLetter())
-console.log('a'.isLetter())
-console.log('X'.isLetter())
-console.log('7'.isLetter())
-console.log('*'.isLetter())
-console.log('ab'.isLetter())
-console.log('a/n'.isLetter())
-
+console.log(maxProduct([4,3,5], 2))
+console.log(maxProduct([10, 8, 7, 9], 3))
