031bb5859caa455850118fbcc099836e73076e1f
Tony Herbert <tonyherbert22@gmail.com>
2022-11-03 12:21:23 -0500

    another daily codewar

--- template.js
+++ template.js
@@ -1,19 +1,38 @@
-// string... Always a string... No #'s... Special Char OKAY
-
-// return sting as V A P O R W A V E -> converts all letters to uppercase & ads 2 spaces.
-
+// sorted Arr of #'s... Always #'s .. Always sorted accending , no strings/funny business
+// target #... Always number
 
 
-function vaporcode(string){
-    // string condensed UPPERCASE together 
-    let upperCase = string.toUpperCase().split(' ').join('')
-    // run a loop, creating new string with 2 spaces between each letter
-    let result = ''
-    for(let letter of upperCase){
-        result += letter + '  '
+// return the index if the target is found.... if not, return index where it would be inserted
+
+
+! This is a solution *NOT using 0(log n)
+var searchInsert = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] >= target) {
+            return i;
+        }
     }
-    return result.trim()
+    return nums.length;
+};
+
+
+let searchInsert  = function(nums, target){
+    let left = 0
+    let right = nums.length
+    while(left < right){
+        const middle = Math.floor((left + right ) / 2)
+        if(nums[middle] < target){
+            left = middle + 1
+        }else{
+            right = middle
+        }
+    }
+    return left
 }
 
 
-console.log(vaporcode('Lets go to the movies'), 'L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S');
+
+console.log(searchInsert([1,3,5,6], 5), '2');
+console.log(searchInsert([1,3,5,6], 2), '1');
+console.log(searchInsert([1,3,5,6], 4), '2');
+console.log(searchInsert([1,3,5,6], 7), '4');
